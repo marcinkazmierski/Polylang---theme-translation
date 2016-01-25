8514037384aad46716209324c5ca7445d4bf4826
Marcin Kaźmierski <marcin.kazmierski@espeo.eu>
2016-01-25 22:22:41 +0100

    Init plugin.

---
+++ polylang-theme-translation.php
@@ -0,0 +1,15 @@
+<?php
+/* Plugin Name: Polylang - theme translation
+Plugin URI: https://github.com/marcinkazmierski/Polylang---theme-translation
+Description: Polylang - theme translation for WordPress
+Version: 1.0
+Author: Marcin Kazmierski
+License: GPL2
+*/
+
+defined('ABSPATH') or die('No script kiddies please!');
+
+class Polylang_Theme_Translation
+{
+    
+}
