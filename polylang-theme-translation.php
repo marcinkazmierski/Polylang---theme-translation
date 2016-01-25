d1956d804539ce4507a13ce2843f3a63fb0a5aaa
Marcin Kaźmierski <marcin.kazmierski@espeo.eu>
2016-01-25 23:14:25 +0100

    Template class.

--- polylang-theme-translation.php
+++ polylang-theme-translation.php
@@ -11,5 +11,34 @@ defined('ABSPATH') or die('No script kiddies please!');
 
 class Polylang_Theme_Translation
 {
-    
+    protected $plugin_path;
+
+    public function __construct()
+    {
+        $this->plugin_path = dirname(__FILE__);
+    }
+
+    public function run()
+    {
+
+    }
+
+    protected function init()
+    {
+
+    }
+
+    protected function get_files_from_dir($dir_name)
+    {
+
+    }
+
+    protected function file_scanner()
+    {
+
+    }
+
 }
+
+$plugin_obj = new Polylang_Theme_Translation();
+$plugin_obj->run();
