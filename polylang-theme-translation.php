697ad53d199062d2291e521119c5c388ff5be8bd
Marcin Kaźmierski <marcin.kazmierski@espeo.eu>
2016-01-26 21:13:18 +0100

    File scanner.

--- polylang-theme-translation.php
+++ polylang-theme-translation.php
@@ -13,27 +13,34 @@ class Polylang_Theme_Translation
 {
     protected $plugin_path;
 
+    protected $files_extensions = array(
+        // 'php',
+        'inc',
+        'twig',
+    );
+
     public function __construct()
     {
         $this->plugin_path = dirname(__FILE__);
     }
 
-    public function run()
+    protected function run()
     {
         $themes = wp_get_themes();
-
         if (!empty($themes)) {
             foreach ($themes as $name => $theme) {
                 $theme_path = $theme->theme_root . DIRECTORY_SEPARATOR . $name;
                 $files = $this->get_files_from_dir($theme_path);
-                // TODO...
+                $strings = $this->file_scanner($files);
+                $this->add_to_polylang_register($strings, $name);
             }
         }
     }
 
-    protected function init()
-    {
 
+    public function init()
+    {
+        $this->run();
     }
 
     /**
@@ -46,7 +53,10 @@ class Polylang_Theme_Translation
         foreach ($files as $key => $value) {
             $path = realpath($dir_name . DIRECTORY_SEPARATOR . $value);
             if (!is_dir($path)) {
-                $results[] = $path;
+                $path_parts = pathinfo($path);
+                if (in_array($path_parts['extension'], $this->files_extensions)) {
+                    $results[] = $path;
+                }
             } else if ($value != "." && $value != "..") {
                 $temp = $this->get_files_from_dir($path);
                 $results = array_merge($results, $temp);
@@ -55,14 +65,25 @@ class Polylang_Theme_Translation
         return $results;
     }
 
-    protected function file_scanner()
+    protected function file_scanner($files)
     {
-
+        $strings = array();
+        foreach ($files as $file) {
+            preg_match_all("/pll_[_e][\s]*\([\s]*[\'\"](.*?)[\'\"][\s]*\)/s", file_get_contents($file), $matches);
+            if (!empty($matches[1])) {
+                $strings = array_merge($strings, $matches[1]);
+            }
+        }
+        return $strings;
     }
 
-    protected function add_to_polylang_register()
+    protected function add_to_polylang_register($strings, $context)
     {
-
+        if (!empty($strings)) {
+            foreach ($strings as $string) {
+                pll_register_string($string, $string, $context);
+            }
+        }
     }
 
     public function install()
@@ -77,5 +98,10 @@ class Polylang_Theme_Translation
 
 }
 
-$plugin_obj = new Polylang_Theme_Translation();
-$plugin_obj->run();
+add_action('init', 'process_polylang_theme_translation');
+
+function process_polylang_theme_translation()
+{
+    $plugin_obj = new Polylang_Theme_Translation();
+    $plugin_obj->init();
+}
